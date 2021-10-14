5edfed862dfde6fd38a27703c41e5e55d2280bb5
Miden0 <midenoprog@gmail.com>
2021-10-14 20:39:33 -0300

    first commit

---
+++ Node/clase1/ejercicio.js
@@ -0,0 +1,10 @@
+var nombre = "Ezequiel"  // Definir nombre
+var apellido = "Fuentes" // Definir apellido
+var edad = 18            // Definir edad
+
+// Imprimir todos los datos concatenados
+console.log(`Nombre completo: ${nombre} ${apellido}. edad: ${edad}`)
+
+// Sumar un numero a la edad y volver a imprimir los datos
+edad++
+console.log(`Nombre completo: ${nombre} ${apellido}. edad: ${edad}`)
